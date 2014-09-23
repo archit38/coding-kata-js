63c4e6cd3ff34a741d73fc0867c4845092434970
archit <architbhardwaj88@gmail.com>
2014-09-23 23:53:46 +0100

    tests
    added different test scenarios

---
+++ tests/RomanGenTest.js
@@ -0,0 +1,32 @@
+describe('MainCtrl', function(){
+  var scope, ctrl;
+
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope.$new();
+    ctrl = $controller(MainCtrl, { $scope: scope });
+  }));
+
+  it('should change Roman Numeral according to entered number < 1000', function() {
+    scope.number = 456;
+    scope.$digest();
+    expect(scope.roman).toBe("CDLVI");
+  });
+  
+  it('should change Roman Numeral according to entered number > 1000', function() {
+    scope.number = 1298;
+    scope.$digest();
+    expect(scope.roman).toBe("MCCXCVIII");
+  });
+  
+  it('should change Roman Numeral according to entered number > 2000', function() {
+    scope.number = 2001;
+    scope.$digest();
+    expect(scope.roman).toBe("MMI");
+  });
+  
+   it('should change Roman Numeral according to entered number > 3000', function() {
+    scope.number = 3999;
+    scope.$digest();
+    expect(scope.roman).toBe("MMMXMIX");
+  });
+});
